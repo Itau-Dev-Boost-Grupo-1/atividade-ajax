e10cc21caf302544208d02745d5a413c53a379f5
Faiton <franklin.veiga@processmind.com.br>
2020-08-29 12:40:48 -0300

    Adicionando o render do endereço

--- src/CepComponent/RenderAddress/index.js
+++ src/CepComponent/RenderAddress/index.js
@@ -1,10 +1,57 @@
-export default function(){
-    
+// CSS classes for the style
+const ADDRESS_LABLE_CSS_CLASS = "label-address"
+const ADDRESS_CSS_CLASS = "address"
+const ADDRESS_FIELD_CSS_CLASS = "address-field"
+
+/**
+ * @name renderAddress
+ * @description Renders the html using the address data
+ *
+ * @param {*} addressData
+ *
+ * @returns HTML DOM object
+ */
+export default function renderAddress(addressData = {}) {
+  let addressContainer = document.createElement("div")
+  addressContainer.setAttribute("class", ADDRESS_CSS_CLASS)
+
+  if (!addressData) {
+    console.error("[renderAddress] The address data was not given.")
+
+    return addressContainer
+  }
+
+  for (const dataName in addressData) {
+
+    const data  = addressData[dataName];
+    const dataField = createDataField(data, dataName)
+
+    addressContainer.appendChild(dataField)
+  }
+
+  return addressContainer
 }
-//Franklin
-// function injectHtml(data){
-//     const element = document.createElement("p")
-//     var text = document.createTextNode(data)
-//     element.appendChild(text)
-//     document.body.appendChild(element)
-// }
+
+function createDataField(data = "", label = "") {
+  let dataFieldContainer = document.createElement("div")
+  
+  if (label.length) {
+    let separator = ":"
+
+    let labelHolder = document.createElement("span")
+    labelHolder.setAttribute("class", ADDRESS_LABLE_CSS_CLASS)
+    labelHolder.appendChild(document.createTextNode(label + separator))
+
+    dataFieldContainer.appendChild(labelHolder)
+  }
+
+  if (data.length) {
+    let dataField = document.createElement("span")
+    dataField.setAttribute("class", ADDRESS_FIELD_CSS_CLASS)
+    dataField.appendChild(document.createTextNode(data))
+
+    dataFieldContainer.appendChild(dataField)
+  }
+
+  return dataFieldContainer
+}
